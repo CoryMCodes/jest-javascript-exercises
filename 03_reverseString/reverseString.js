d8e3f2720b189b59c18b95a6a0019479849ced76
Cory Musick <cory.musick37@gmail.com>
2023-04-21 11:14:46 -0500

    feat: completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  // split string to arary
+  let splitString = string.split("");
+  // initialize empty string to take reversed string
+  let reverseString = "";
+  // loop as many times as there is chars in array
+  for(i= splitString.length - 1; i >= 0; i--){
+    // add character to reverse string starting with last index of splitString[]
+    reverseString += splitString[i]; 
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
