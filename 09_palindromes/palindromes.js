d251ba18ffdf62b8fbf9126dc71a8dd7afb28bd5
Cory Musick <cory.musick37@gmail.com>
2023-04-21 14:25:40 -0500

    feat: palindroms exercise complete; used some usefull regex code as well as the benefit of cleaning string inputs for comparision regardless of things like upper/lowercase and punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // step1: clean input
+  //removes punctuation and white space to just compare characters
+  const lowerCasePunctuationRE = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{1,}/g, "").toLowerCase();
+  const splitString = lowerCasePunctuationRE.split("");
+  // step 2: reverse clean string
+  let reverseString = "";
+  // loop as many times as there is chars in array
+  for(i= splitString.length - 1; i >= 0; i--){
+    // add character to reverse string starting with last index of splitString[]
+    reverseString += splitString[i]; 
+  }
+  // compare reversed string to cleaned input string 
+  return reverseString === lowerCasePunctuationRE ? true : false;
 };
 
 // Do not edit below this line
