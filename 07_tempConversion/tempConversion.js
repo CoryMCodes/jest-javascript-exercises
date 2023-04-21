dc3c00ff796532bbcdd7fe5b2b7bf08fd6136dba
Cory Musick <cory.musick37@gmail.com>
2023-04-21 13:31:15 -0500

    feat: tempConversion exercise complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  // use Math.round ( * x) / x to determine decimal places
+  // use this method because parseFloat returns a string
+  let converted = Math.round(((num - 32) * (5/9)) * 10) / 10;
+  return converted;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let converted = Math.round((num * (9/5) + 32) * 10)/10;
+  return converted;
 };
 
 // Do not edit below this line
