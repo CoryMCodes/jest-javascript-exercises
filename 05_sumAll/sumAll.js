e7f5cad7de8f88efeb9d99a5a88d84d01691d52a
Cory Musick <cory.musick37@gmail.com>
2023-04-21 12:46:19 -0500

    feat: exercise 05 sumAll compelete, practicing adding comments to code in order to undertstand why i write the code i do

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  let small;
+  let large;
+  // Assign given numbers to correct position to work with for loop
+  if(num1 < num2){
+    small = num1;
+    large = num2
+  }else{
+    small = num2;
+    large = num1;
+  }
 
+  // Catch edge cases that do not work in function
+  if(typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0){
+    return "ERROR"
+  }
+  // loop from smallest to largest number adding all number inbetween to sum variable
+  for(i = small; i <= large; i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
