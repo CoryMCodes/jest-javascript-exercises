e56bc072a215dcdafdc11a1637af0c1731fa5bc7
Cory Musick <cory.musick37@gmail.com>
2023-04-21 12:34:17 -0500

    feat: remove from array exercsie complete; this exercise was difficult but learned a new code set using a combination of array.filter and array.find to filter an array of arguments out of a given array; also lelearned how to use ..args in function declarations

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...theArgs) {
+  // filter given array i is each index in array
+  return array.filter(i => {
+    // iterate through args to to see if theres a match. ! from true to false to remove using filter
+    return !theArgs.find(j =>{
+      // use strict type matching to pass last test
+      return j === i;
+    })
+    })
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
