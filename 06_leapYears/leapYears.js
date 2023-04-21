785046e2f4193b0c0465179cb9b9651f56e365e9
Cory Musick <cory.musick37@gmail.com>
2023-04-21 12:56:36 -0500

    feat: leapyear exercise complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // if its not divisble by 4 or if it is divisible by 100 but not 400 its not a leap year
+  if(year % 4 != 0 || year % 100 === 0 && year % 400 != 0){
+    return false;
+  }else{
+    return true;
+}
 };
 
 // Do not edit below this line
