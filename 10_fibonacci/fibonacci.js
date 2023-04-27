e2c466deeb96b12cb45d863b031b048bfa855366
Cory Musick <cory.musick37@gmail.com>
2023-04-27 10:26:18 -0500

    feat: Jest Testing exercises complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(fibNumber) {
+  if (typeof fibNumber === "string") fibNumber = Number(fibNumber);
+  
+  if (fibNumber < 0) return "OOPS";
 
+  let current = 0;
+  let previous = 1;
+  let next = fibNumber;
+
+  for(i = 2; i<= fibNumber; i++){
+    next = current + previous;
+    current = previous;
+    previous = next;
+  }
+  return next;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = function(person){
+  let currentYear = new Date().getFullYear();
+  if (person.hasOwnProperty("yearOfDeath")){
+    return person.yearOfDeath - person.yearOfBirth;
+  }else{
+    return currentYear - person.yearOfBirth
+  }
+}
 
+const findTheOldest = function(people) {
+  let oldestPerson = people.reduce(function(oldest, person) {
+     return (getAge(oldest) || 0) > (getAge(person)) ? oldest : person;
+  }, {})
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
